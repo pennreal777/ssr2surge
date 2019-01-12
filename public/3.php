3db7f527ba13593937e1f3890fd1e76f267d9f26
Penn Real <liupeng1502@gmail.com>
2019-01-13 01:33:52 +0800

    优化下显示，还没搞明白怎么设置surge显示名称，凑合用着

--- public/3.php
+++ public/3.php
@@ -2,7 +2,7 @@
 $host = $_SERVER['HTTP_HOST'];
 $tmp =  $_SERVER['PHP_SELF'];
 $host = $host.substr($tmp,0,strlen($tmp)-5);
-$url = "http://$host"."4.php?".$_SERVER["QUERY_STRING"] ;
+$url = "http://$host"."prCloud.php?".$_SERVER["QUERY_STRING"] ;
 $link = "surge:///install-config?url=".urlencode($url);
 echo "<a href='$link'>Safari浏览器直接点我，其他浏览器长按复制到Safari中浏览</a>"
 ?>
